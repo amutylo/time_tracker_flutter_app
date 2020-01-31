f070c852a97cdb8d52b86d9a2eb654140abd265e
Andrii Mutylo <amutylo@webcodin.com>
2020-01-31 18:24:00 +0200

    update sign in page with the structure

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -6,7 +6,44 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Time tracker'),
+        elevation: 2.0,
       ),
+      body: _buildContent()
     );
+  }
+
+  // Build content for the body of scaffold widget
+  Widget _buildContent() {
+    return Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Sign in',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.w600,
+              )
+              ),
+            SizedBox(height: 8.0),
+            Container(
+              color: Colors.blueAccent,
+              child: SizedBox(
+                height: 100.0,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Container(
+              color: Colors.purple,
+              child: SizedBox(
+                height: 100.0,
+              ),
+            ),
+          ],
+        ),
+      );
   }
 }
