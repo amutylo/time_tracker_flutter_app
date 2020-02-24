83983c8ef68fb3225fcccd320a1dd87063e720f5
Andrii Mutylo <amutylo@webcodin.com>
2020-02-24 15:27:36 +0200

    update google sign out

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -75,6 +75,8 @@ class Auth implements AuthBase {
 
   @override
   Future<void> signOut() async {
+    final googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
     await _firebaseAuth.signOut();
   }
 }
