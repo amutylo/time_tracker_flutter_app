f4ade85ee7821358b59291134a62d5aad140d70b
Andrii Mutylo <amutylo@webcodin.com>
2020-02-24 18:07:07 +0200

    update email sign in page

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_app/common_widgets/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _submit() {
+
+  }
 
   List<Widget> _buildChildren() {
     return [
       TextField(
+        controller: _emailController,
         decoration: InputDecoration(
           labelText: 'Email',
           hintText: 'test@test.com',
@@ -12,15 +21,16 @@ class EmailSignInForm extends StatelessWidget {
       ),
       SizedBox(height: 8.0),
       TextField(
+        controller: _passwordController,
         decoration: InputDecoration(
           labelText: 'Password',
         ),
         obscureText: true
       ),
       SizedBox(height: 8.0),
-      RaisedButton(
-        child: Text('SignIn'),
-        onPressed: () {}
+      FormSubmitButton(
+        text: 'SignIn',
+        onPressed: _submit
       ),
       SizedBox(height: 8.0),
       FlatButton(
